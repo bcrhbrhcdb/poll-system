5b2d2d0b630ad6dd876655f8832775bf94ae7975
bcrhbrhcdb <apet2804@mpsedu.org>
2024-10-07 09:30:22 -0500

    j

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.38.4/+esm'
 
 const supabaseUrl = 'https://ovfalbwdefhczybpihca.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92ZmFsYndkZWZoY3p5YnBpaGNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzMDQ2MTksImV4cCI6MjA0Mzg4MDYxOX0.JO-n6L1S0Jw3UY-amCBCIQTrYHfUHMC52JdZakRcGXk'
